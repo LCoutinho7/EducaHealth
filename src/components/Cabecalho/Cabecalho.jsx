fc65e7bd7c7c7a46d5156823c1ff2319d4ee9953
LCoutinho7 <rm551617@fiap.com.br>
2023-11-24 18:39:12 -0300

    Estilizando a HOME

---
+++ src/components/Cabecalho/Cabecalho.jsx
@@ -0,0 +1,30 @@
+"use client";
+import Link from "next/link";
+import Image from "next/image";
+
+export default function Cabecalho() {
+
+  if (sessionStorage.getItem("token-user")) {
+    return (
+      
+      <header className="bg-009ff7 text-white shadow-lg text-center">
+
+      </header>
+    );
+  } else {
+    return (
+      <header className="bg-azulCabecalho shadow-lg text-center shadow-custom">
+        <div className="mx-auto max-w-2xl flex items-center justify-center flex-col">
+            <Image src="/logo_educahealth.png" alt="Logo Educahealth" width={100} height={90}/>
+        </div>
+
+      <button className="absolute top-0 right-0 mt-2 mr-2 text-black px-4 pb-6 rounded">
+        <Link href="/login">Login</Link>
+      </button>
+
+      </header>
+    );
+  }
+}
+
+
